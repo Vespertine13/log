985f43bffee8906f07b1ffdd1da76fec139288bf
Eivind Kjeka Broen <60471005+Vespertine13@users.noreply.github.com>
2023-07-06 16:11:32 +0200

    Create config.R

---
+++ config.R
@@ -0,0 +1,12 @@
+# Paths
+log_path <- "workout_log.txt"
+data_path <- "data/log/workout_log.csv"
+output_path <- "workout_log.png"
+
+# Exercises
+exercises <- c("bench_120kg",
+               "dips_40kg",
+               "biceps_curls_15kg",
+               "pullups_10kg",
+               "squats_120kg",
+               "straight_wheel")
