876f8931ae9988fa051af905938d4bec04362627
Eivind Kjeka Broen <60471005+Vespertine13@users.noreply.github.com>
2023-08-30 17:39:55 +0200

    Update config.R

--- config.R
+++ config.R
@@ -6,7 +6,7 @@ output_path <- "workout_log.png"
 # Exercises
 exercises <- c("bench_120kg",
                "dips_40kg",
-               "biceps_curls_15kg",
-               "pullups_10kg",
+               "biceps_curls_20kg",
+               "pullups_15kg",
                "squats_120kg",
                "straight_wheel")
